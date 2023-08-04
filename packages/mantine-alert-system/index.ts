4abf75cb342efc4b80e29b9a6f4e26eb40ed65a9
Irfan Vigma Taufik <davigmacode@gmail.com>
2023-08-04 08:23:58 +0700

    [alert-system] changed transitions file name

--- packages/mantine-alert-system/index.ts
+++ packages/mantine-alert-system/index.ts
@@ -2,5 +2,5 @@ export * from './src/use-alert-stack-state';
 export * from './src/use-alert-stack';
 export * from './src/use-alert-switch';
 export * from './src/use-alert-severity';
-export * from './src/transition';
+export * from './src/transitions';
 export * from './src/types';
