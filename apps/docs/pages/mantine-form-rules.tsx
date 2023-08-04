2e5ca8fe6e2f0f5d1324aef996fbb9799850aa78
Irfan Vigma Taufik <davigmacode@gmail.com>
2023-08-04 08:25:07 +0700

    [docs] added form rules example

---
+++ apps/docs/pages/mantine-form-rules.tsx
@@ -0,0 +1,54 @@
+import { PasswordInput, Box, Button } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import rules from 'mantine-form-rules';
+
+export default function Page() {
+  const form = useForm({
+    validateInputOnChange: true,
+    validateInputOnBlur: true,
+    initialValues: {
+      currentPassword: '',
+      newPassword: '',
+      newPasswordConfirm: '',
+      logout: false,
+    },
+    validate: {
+      currentPassword: rules.isRequired(),
+      newPassword: rules([
+        rules.isRequired(),
+        rules.hasUpperCase(),
+        rules.hasNumber(),
+        rules.minLength(6),
+      ], { accumulate: true }),
+      newPasswordConfirm: rules([
+        rules.isRequired(),
+        rules.matchesField('newPassword', 'Passwords are not the same'),
+      ]),
+    },
+  });
+
+  return (
+    <Box w={300} mx="auto" mt="xl">
+      <form onSubmit={form.onSubmit((values) => console.log(values))}>
+        <PasswordInput
+          label="Current Password"
+          placeholder="Your password"
+          {...form.getInputProps('currentPassword')}
+        />
+        <PasswordInput
+          label="New Password"
+          placeholder="Your password"
+          mt="sm"
+          {...form.getInputProps('newPassword')}
+        />
+        <PasswordInput
+          label="Confirm New Password"
+          placeholder="Your password"
+          mt="sm"
+          {...form.getInputProps('newPasswordConfirm')}
+        />
+        <Button type="submit" mt="lg">Submit</Button>
+      </form>
+    </Box>
+  );
+}
